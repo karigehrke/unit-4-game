1fc2bc7e4550d5f69ab27dc96095ffc4de98f793
Vesper <no-reply>
2018-10-19 21:31:56 -0400

    basic code outline

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,8 @@
-var words = ["ball", "cleats", "jersey"];
-var wins = 0;
-var guessesLeft = 5;
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var answerTrack = [];
+var winTally = 0;
+var loseTally = 0;
+var crystalValue = 0;
+var computerNum = 0;
+var userGuess = 0;
 
-
-var computerChoice = words[Math.floor(Math.random() * words.length)];
-
-for (var i = 0; i < computerChoice.length; i++) {
-    answerTrack[i] = "_";
-};
-
-console.log(answerTrack);
+var computerNum = Math.floor(Math.random() * 120) + 19;
+console.log(computerNum);
